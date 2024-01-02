dcc5460a7b976a716c9ba3ad9039c89c520ea8ef
CrimeMaster <gtirkey300@gmail.com>
2024-01-02 11:08:05 +0530

    client working locally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import JobPage from "./pages/jobPage/JobPage";
+import Alljob from "./pages/allJob/Alljob";
+import Jobdes from "./components/jobdes/Jobdes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/jobPage" element={<JobPage />} />
+          <Route path="/header" element={<Alljob />} />
+          <Route path="/jobdes" element={<Jobdes />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
